728349f2dc3cb2f179bbd629d0d338d717d35897
Pedro Henrique Alves de Freitas <leonkene18@gmail.com>
2023-05-31 10:49:45 -0300

    aa

--- Controllers/EquipeController.cs
+++ Controllers/EquipeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ProjetoGamer_Banco_de_dados.Infra;
+using ProjetoGamer_Banco_de_dados.Models;
 
 namespace ProjetoGamer_Banco_de_dados.Controllers
 {
@@ -30,6 +31,48 @@ namespace ProjetoGamer_Banco_de_dados.Controllers
 
             //retorna a view de equipe (tela)
             return View();
+        }
+
+        [Route("Cadastrar")] //https://localhost/Equipe/Cadastrar
+        public IActionResult Cadastrar(IFormCollection form)
+        {
+            Equipe novaEquipe = new Equipe();
+
+            novaEquipe.Nome = form["Nome"].ToString();
+
+            //novaEquipe.Imagem = form["Imagem"].ToString();
+            if (form.Files.Count > 0)
+            {
+                var file = form.Files[0];
+
+                var folder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/Equipes");
+
+                if (!Directory.Exists(folder))
+                {
+                    Directory.CreateDirectory(folder);
+                }
+
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/", folder, file.FileName);
+
+                using (var stream = new FileStream(path,FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+
+                novaEquipe.Imagem = file.FileName;
+            }
+            else
+            {
+                novaEquipe.Imagem = "padrao.png";
+            }
+
+            c.Equipe.Add(novaEquipe);
+
+            c.SaveChanges();
+
+            ViewBag.Equipe = c.Equipe.ToList();
+
+            return LocalRedirect("~/Equipe/Listar");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
